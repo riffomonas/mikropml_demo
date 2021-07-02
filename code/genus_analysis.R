25c51ea1fa293179626a586dee782a4da5656c18
Pat Schloss <pschloss@umich.edu>
2021-07-02 09:57:24 -0400

    Create ROC curves for individual biomarkers

--- code/genus_analysis.R
+++ code/genus_analysis.R
@@ -80,3 +80,63 @@ composite %>%
   )
 
 ggsave("figures/significant_genera.tiff", width=6, height=4)
+
+get_sens_spec <- function(threshold, score, actual, direction){
+  
+  # threshold <- 100
+  # score <- test$score
+  # actual <- test$srn
+  # direction <- "greaterthan"
+  
+  predicted <- if(direction == "greaterthan") {
+    score > threshold 
+    } else {
+      score < threshold
+    }
+  
+  tp <- sum(predicted & actual)
+  tn <- sum(!predicted & !actual)
+  fp <- sum(predicted & !actual)
+  fn <- sum(!predicted & actual)  
+  
+  specificity <- tn / (tn + fp)
+  sensitivity <- tp / (tp + fn)
+  
+  tibble("specificity" = specificity, "sensitivity" = sensitivity)
+}
+
+get_roc_data <- function(x, direction){
+  
+  # x <- test
+  # direction <- "greaterthan"
+  
+  thresholds <- unique(x$score) %>% sort()
+  
+  map_dfr(.x=thresholds, ~get_sens_spec(.x, x$score, x$srn, direction)) %>%
+    rbind(c(specificity = 0, sensitivity = 1))
+  
+}
+
+# get_sens_spec(100, test$score, test$srn, "greaterthan")
+# get_roc_data(test, "greaterthan")
+
+roc_data <- composite %>%
+  inner_join(sig_genera, by="taxonomy") %>%
+  select(group, taxonomy, rel_abund, fit_result, srn) %>%
+  pivot_wider(names_from=taxonomy, values_from=rel_abund) %>%
+  pivot_longer(cols=-c(group, srn), names_to="metric", values_to="score") %>%
+  # filter(metric == "fit_result") %>%
+  nest(data = -metric) %>%
+  mutate(direction = if_else(metric == "Lachnospiraceae_unclassified",
+                             "lessthan","greaterthan")) %>%
+  mutate(roc_data = map2(.x = data, .y=direction, ~get_roc_data(.x, .y))) %>%
+  unnest(roc_data) %>%
+  select(metric, specificity, sensitivity)
+
+roc_data %>%
+  ggplot(aes(x=1-specificity, y=sensitivity, color=metric)) +
+  geom_line() +
+  geom_abline(slope = 1, intercept = 0, color="gray") +
+  theme_classic()
+
+ggsave("figures/roc_figure.tiff", width=6, height=4)
