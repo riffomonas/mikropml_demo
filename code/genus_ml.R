d0d2bc8510c424b24bb4c5f3544c05dad976a8e7
Pat Schloss <pschloss@umich.edu>
2021-07-09 13:25:22 -0400

    Modify hyperparameters

--- code/genus_ml.R
+++ code/genus_ml.R
@@ -11,10 +11,32 @@ srn_genus_data <- composite %>%
 srn_genus_preprocess <- preprocess_data(srn_genus_data,
                                         outcome_colname = "srn")$dat_transformed
 
-srn_genus_results <- run_ml(srn_genus_preprocess,
+test_hp <- list(alpha = 0, 
+                lambda = c(0.1, 1, 2, 3, 4, 5, 10))
+
+get_srn_genus_results <- function(seed){
+  
+  run_ml(srn_genus_preprocess,
        method="glmnet",
        outcome_colname = "srn",
        kfold = 5,
-       cv_times = 100,
+       cv_times = 10,
        training_frac = 0.8,
-       seed = 19760620)
+       hyperparameters = test_hp, 
+       seed = seed)
+
+}
+
+iterative_run_ml_results <- map(c(1,2,3), get_srn_genus_results)
+
+performance <- iterative_run_ml_results %>%
+  map(pluck, "trained_model") %>%
+  combine_hp_performance()
+
+plot_hp_performance(performance$dat, lambda, AUC)
+
+performance$dat %>%
+  group_by(alpha, lambda) %>%
+  summarize(mean_AUC = mean(AUC), .groups="drop") %>%
+#  top_n(n=3, mean_AUC)
+  ggplot(aes(x= lambda, y=mean_AUC, color=as.character(alpha))) + geom_line()
