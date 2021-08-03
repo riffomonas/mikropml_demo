2b3268326143d97631b8ae94e25f42035029b8e0
Pat Schloss <pschloss@umich.edu>
2021-08-03 10:30:59 -0400

    Feature importance of L2 regularized logistic regression

---
+++ feature_importance.R
@@ -0,0 +1,64 @@
+library(tidyverse)
+
+l2_files <- list.files(path="processed_data",
+                       pattern="l2_genus_\\d*.Rds",
+                       full.names=TRUE)
+
+get_weights <- function(file_name){
+  
+  model <- readRDS(file_name) %>%
+    pluck("trained_model")
+  
+  coef(model$finalModel, model$bestTune$lambda) %>%
+    as.matrix %>%
+    as_tibble(rownames="feature") %>%
+    rename(weight = `1`) %>%
+    mutate(seed = str_replace(file_name,
+                              "processed_data/l2_genus_(\\d*).Rds",
+                              "\\1"))
+
+}
+
+l2_weights <- map_dfr(l2_files, get_weights)
+
+l2_weights %>%
+  filter(feature != "(Intercept)") %>%
+  group_by(feature) %>%
+  summarize(median = median(weight),
+            l_quartile = quantile(weight, prob=0.25),
+            u_quartile = quantile(weight, prob=0.75)) %>%
+  mutate(feature = fct_reorder(feature, median)) %>%
+  filter(abs(median) > 0.01) %>%
+  ggplot(aes(x=median, y=feature, xmin=l_quartile, xmax=u_quartile)) +
+  geom_point() +
+  geom_linerange()
+  
+ggsave("figures/l2_weights.tiff", width=5, height=5)
+
+
+
+get_feature_importance <- function(file_name){
+  
+  feature_importance <- readRDS(file_name) %>%
+    pluck("feature_importance") %>%
+    as_tibble() %>%
+    select(names, perf_metric, perf_metric_diff)
+  
+}
+
+l2_feature_importance <- map_dfr(l2_files, get_feature_importance)
+
+
+l2_feature_importance %>%
+  rename(feature = names) %>%
+  group_by(feature) %>%
+  summarize(median = median(perf_metric_diff),
+            l_quartile = quantile(perf_metric_diff, prob=0.25),
+            u_quartile = quantile(perf_metric_diff, prob=0.75)) %>%
+  mutate(feature = fct_reorder(feature, median)) %>%
+  filter(median > 0.0025) %>%
+  ggplot(aes(x=median, y=feature, xmin=l_quartile, xmax=u_quartile)) +
+  geom_point() +
+  geom_linerange()
+
+ggsave("figures/l2_feature_importance.tiff", width=5, height=5)
